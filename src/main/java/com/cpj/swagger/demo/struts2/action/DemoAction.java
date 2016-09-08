ddc6620c2936db56dd492a3386b29804f380490b
雍欢 <2232911026@qq.com>
2016-09-08 14:59:41 +0800

    增量更新

--- src/main/java/com/cpj/swagger/demo/struts2/action/DemoAction.java
+++ src/main/java/com/cpj/swagger/demo/struts2/action/DemoAction.java
@@ -5,10 +5,19 @@
  */
 package com.cpj.swagger.demo.struts2.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 
+import com.alibaba.fastjson.JSONWriter;
 import com.cpj.swagger.annotation.API;
 import com.cpj.swagger.annotation.APIs;
+import com.cpj.swagger.annotation.Param;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
@@ -16,20 +25,31 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 @APIs("/demo")
 @Namespace("/demo")
+@Action("demoAction")
 public class DemoAction extends ActionSupport {
 	
-	@API(value="demo1", summary="示例1")
-	public void demo1() throws Exception {
-		
+	@API(value="demoAction!login", summary="示例1", parameters={
+			@Param(name="username", description="用户名", type="string"),
+			@Param(name="password", description="密码", type="string", format="password")
+	})
+	public void login() throws Exception {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("application/json;charset=utf-8");
+		JSONWriter writer = new JSONWriter(response.getWriter());
+		Map<String, String> user = new HashMap<String, String>();
+		user.put("username", username);
+		user.put("password", password);
+		writer.writeObject(user);
+		writer.flush();
+		writer.close();
 	}
 	
-	@API(value="demo2", summary="示例2")
-	public void demo2() throws Exception {
-		
+	private String username;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	
-	@API(value="demo3", summary="示例3")
-	public void demo3() throws Exception {
-		
+	private String password;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
